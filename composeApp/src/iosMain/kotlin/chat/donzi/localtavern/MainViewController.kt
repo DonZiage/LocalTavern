ab31eb9d203b8cd222a25c74b360e72d9aeaa0d8
DonZiage <zzgrtx@gmail.com>
2026-05-19 14:29:36 -0700

    MainViewController warning override

--- composeApp/src/iosMain/kotlin/chat/donzi/localtavern/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/chat/donzi/localtavern/MainViewController.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.window.ComposeUIViewController
 import chat.donzi.localtavern.ui.App
 import chat.donzi.localtavern.data.database.DriverFactory
 
+@Suppress("unused")
 fun MainViewController() = ComposeUIViewController {
 
     App(driverFactory = DriverFactory())
